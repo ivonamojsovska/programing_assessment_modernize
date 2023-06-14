3d914d16886756cb598f75cd6f99804a1058707f
ivonamojsovska <ivonamojsovska15@gmail.com>
2023-06-13 20:31:03 -0700

    form phone input masked

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const phone = document.getElementById('phone-number')
+
+phone.addEventListener('input', (e) => {
+    const input = e.target;
+    const value = input.value.replace(/\D/g, '').match(/(\d{3})(\d{3})(\d{4})/);
+
+    if (value) {
+        input.value = !value[2] ? value[1] : `(${value[1]}) ${value[2]}${value[3] ? '-' + value[3] : ''}`;
+    }
+});
