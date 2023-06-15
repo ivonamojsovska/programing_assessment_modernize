0818418c4f66976673993fb695f92f0411199e90
ivonamojsovska <ivonamojsovska15@gmail.com>
2023-06-15 14:12:33 -0700

    form validation added

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 const phone = document.getElementById('phone-number')
 const form = document.querySelector('form')
 const submitButton = document.querySelector('.submit-button');
-
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const borderColorError = '#D50303'
 
 
 ////US mask phone number////
@@ -18,34 +20,73 @@ phone.addEventListener('input', (e) => {
     }
 });
 
-////Submiting form and sending data to specific URL/////
+
+////Change the border color to #D50303 when there is an error in the form's inputs
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault(); // Prevent the form from submitting normally
+    e.preventDefault();
 
-    // Collect form data
-    const formData = new FormData(form);
+    // Reset error styles
+    nameInput.style.borderColor = '';
+    phone.style.borderColor = '';
+    emailInput.style.borderColor = '';
 
-    // Make a POST request to the server
-    fetch('https://formsws-hilstaging-com-0adj9wt8gzyq.runscope.net/solar', {
-        method: 'POST',
-        body: formData
-    })
-        .then(response => {
-            if (response.ok) {
-                // Successful submission
-                console.log('Form submitted successfully');
-                // Change button copy to "Submitted"
-                submitButton.textContent = 'Submitted';
-                submitButton.disabled = true; // Disable the button to prevent further submissions
+    // Perform form validation
+    let hasError = false;
 
-            } else {
-                // Handle errors or unsuccessful submission
-                console.log('Form submission failed');
-            }
+
+    if (nameInput.value.length < 2) {
+        nameInput.style.borderColor = borderColorError;
+        hasError = true;
+    }
+
+    const phoneRegex = /^\(\d{3}\) \d{3}-\d{4}$/;
+    if (!phoneRegex.test(phone.value)) {
+        phone.style.borderColor = borderColorError;
+        hasError = true;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailInput.value)) {
+        console.log(emailInput.value, 'email has to have @')
+        emailInput.style.borderColor = borderColorError;
+        hasError = true;
+    }
+
+    if (hasError) {
+        //Handle form submission error
+        return;
+    }
+
+    // Submit the form if there are no errors
+    if (!hasError) {
+        // Collect form data
+        const formData = new FormData(form);
+
+        // Make a POST request to the server
+        fetch('https://formsws-hilstaging-com-0adj9wt8gzyq.runscope.net/solar ', {
+            method: 'POST',
+            body: formData
         })
-        .catch(error => {
-            // Handle network errors
-            console.log('An error occurred during form submission:', error);
-        });
+            .then(response => {
+                if (response.ok) {
+                    // Successful submission
+                    console.log('Form submitted successfully');
+                    // Reset the form 
+                    form.reset()
+                    // Change button copy to "Submitted"
+                    submitButton.textContent = 'Submitted';
+                    submitButton.disabled = true; // Disable the button to prevent further submissions
+
+                } else {
+                    // Handle errors or unsuccessful submission
+                    console.log('Form submission failed');
+                }
+            })
+            .catch(error => {
+                // Handle network errors
+                console.log('An error occurred during form submission:', error);
+            });
+    }
+
 });
